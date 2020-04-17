67ca84965bc7af3e90bc59fa02731fe8e3c029c8
Shrey Dubey <wshrey09@gmail.com>
2020-04-17 23:07:44 +0530

    update our menu components to work with HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {Route, Switch,Link} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+
+const RandomPage = (props) =>{
+  console.log(props);
+  return(
+    <div>
+      <h1>{props.match.params.word} Page</h1>
+    </div>
+  )
+}
 function App() {
   return (
+    
     <div className="App">
-      <HomePage/>
+    <Switch>
+        <Route exact path = "/" component = {HomePage}/>
+        <Route path = "/:word" component = {RandomPage}/>
+        
+    </Switch>    
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import "./menu-item.styles.scss"
-const MenuItem = ({title,imageUrl,size})=>(
-    <div className = {`${size} menu-item`}>
+import {withRouter} from 'react-router-dom';
+const MenuItem = ({title,imageUrl,size,history,match,linkUrl})=>{
+    console.log(match)
+    console.log(linkUrl);
+    return(
+    <div className = {`${size} menu-item`} onClick = {()=>history.push(`${match.url}${linkUrl}`)}>
         <div 
             className = "background-image"
             style = {{
@@ -12,5 +16,5 @@ const MenuItem = ({title,imageUrl,size})=>(
                     <span className = "subtitle">Shop Now</span>
                 </div>
             </div>
-)
-export default MenuItem;
+)}
+export default withRouter(MenuItem);
