bac0d6ba76efb4fb3a45a8a4ccf56402ddf3a061
Shrey Dubey <wshrey09@gmail.com>
2020-04-18 12:46:58 +0530

    updating app with shop page and styling it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import {Route, Switch,Link} from 'react-router-dom';
-
+import ShopPage from "./pages/shop/shop.component"
 import HomePage from './pages/homepage/homepage.component';
 
-const RandomPage = (props) =>{
-  console.log(props);
-  return(
-    <div>
-      <h1>{props.match.params.word} Page</h1>
-    </div>
-  )
-}
+
 function App() {
   return (
     
     <div className="App">
     <Switch>
         <Route exact path = "/" component = {HomePage}/>
-        <Route path = "/:word" component = {RandomPage}/>
         
+        <Route exact path = "/shop" component = {ShopPage}/>
     </Switch>    
     </div>
   );
