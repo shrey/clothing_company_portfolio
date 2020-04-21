a275b23a29773fa0b07a80981c736b0b436f8f6a
Shrey Dubey <wshrey09@gmail.com>
2020-04-21 22:49:08 +0530

    updated shop data and added cart utils to keep count and avoid repititions

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,17 +1,18 @@
 import {CartActionTypes} from "./cart.types"
+import {addItemToCart} from "./cart.utils"
 const INITIAL_STATE = {
     hidden: true,
      cartItems: []
 }
 const cartReducer = (state = INITIAL_STATE, action) =>{
-    switch(action.type){
+    switch(action.type){ 
         case CartActionTypes.TOGGLE_CART_HIDDEN: return{
             ...state,
             hidden: !state.hidden
         }
         case CartActionTypes.ADD_ITEM : return{
             ...state,
-            cartItems: [...state.cartItems,action.payload] 
+            cartItems: addItemToCart(state.cartItems,action.payload)
         }
         default: return state
     }
