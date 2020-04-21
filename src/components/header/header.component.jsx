89ced3fc67d277a11f884bee342fa3fa7a3a6866
Shrey Dubey <wshrey09@gmail.com>
2020-04-21 18:18:36 +0530

    adding dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,9 +6,10 @@ import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
+import CartIcon from '../cart-icon/cart-icon.component'
 import './header.styles.scss';
-
-const Header = ({ currentUser }) => (
+import CartDropdown from "../cart-dropdown/cart-dropdown.component"
+const Header = ({ currentUser,hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -29,12 +30,19 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <CartIcon/>
     </div>
+    
+       { hidden?
+     null:
+     (<CartDropdown/>)
+    }
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
