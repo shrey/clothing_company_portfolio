17dd3ff151ebe4b5c8102f0c5de56af13ba9bc8a
Shrey Dubey <wshrey09@gmail.com>
2020-04-22 12:10:57 +0530

    adding the number in cart Icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,13 +3,16 @@ import './cart-icon.styles.scss'
 import {ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg"
 import { connect } from 'react-redux'
 import {toggleCartHidden} from "../../redux/cart/cart.action"
-const CartIcon = ({toggleCartHidden})=>(
+const CartIcon = ({toggleCartHidden,itemNumber})=>(
     <div className = "cart-icon" onClick = {toggleCartHidden}>
         <ShoppingIcon className = "shopping-icon"/>
-        <span className = "item-count">0</span>
+        <span className = "item-count">{itemNumber}</span>
     </div>
 )
 const mapDispatchToProps = dispatch=>({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
-export default connect (null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({cart: {itemCount}}) =>({
+    itemNumber: itemCount
+})
+export default connect (mapStateToProps,mapDispatchToProps)(CartIcon);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -2,7 +2,8 @@ import {CartActionTypes} from "./cart.types"
 import {addItemToCart} from "./cart.utils"
 const INITIAL_STATE = {
     hidden: true,
-     cartItems: []
+     cartItems: [],
+     itemCount: 0
 }
 const cartReducer = (state = INITIAL_STATE, action) =>{
     switch(action.type){ 
@@ -12,7 +13,8 @@ const cartReducer = (state = INITIAL_STATE, action) =>{
         }
         case CartActionTypes.ADD_ITEM : return{
             ...state,
-            cartItems: addItemToCart(state.cartItems,action.payload)
+            cartItems: addItemToCart(state.cartItems,action.payload),
+            itemCount: state.itemCount + 1
         }
         default: return state
     }
